51034638f0d08d28d3a5d410f8818aee48d4d64a
Gavin Barham <gavinbarham@yahoo.com>
2023-06-06 14:34:00 -0500

    create custom log enhancer.

---
+++ index.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware, bindActionCreators, compose } from "redux";
+const add = (state = initialState, action) => {
+    if (action.type === "INCREMENT") {
+        return state + 1
+    }
+    return state
+}
+const stateLogEnhancer = (createStore) => (reducer, initialState, enhancer) => {
+    const stateLogReducer = (state = initialState, action) => {
+        console.log(state);
+        const newState = reducer(state, action);
+        console.log(state);
+
+        return newState;
+    };
+    return createStore(stateLogReducer, initialState, enhancer);
+};
+const store = createStore(add, 2, stateLogEnhancer);
+store.dispatch({ type:"INCREMENT" })
+console.log(store.getState());
